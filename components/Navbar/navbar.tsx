1c0de09080306aa764519be938323567a0db4a12
Kaveh <kavehkhorshidiii@gmail.com>
2026-07-02 12:54:33 +0330

    commit

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,16 +1,18 @@
 'use client'
 // Imports
 import Spinner from '../spinner/spinner';
-import { redirect , usePathname } from 'next/navigation';
-import { ArrowLeft, ArrowRightIcon, CircleCheck, MoreHorizontal } from 'lucide-react';
+import { redirect, usePathname } from 'next/navigation';
+import { ArrowLeft, ArrowRightIcon, CircleCheck, X, MoreHorizontal } from 'lucide-react';
 import { useState } from 'react';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import ChangeColorNavbar from "@/components/Modals/changeColorNavbar/changeColorNavbar"
 import { useQuery } from "@tanstack/react-query";
+import Image from 'next/image';
+import { Card } from '../ui/card';
 
 
-export default function Navbar({ boardTitle }: { boardTitle: string }) {
+export default function Navbar({ boardTitle }: { boardTitle?: string }) {
 
 
    const PathName = usePathname(); const splitPathName = PathName.split("/"); const boardID = splitPathName[2];  // Path Name
@@ -18,6 +20,7 @@ export default function Navbar({ boardTitle }: { boardTitle: string }) {
    const isDashboardPage = PathName === "/dashboard";  // dashboard page
    const isBoardPage = PathName.startsWith("/boards");  // board/...
    const [isModal, setIsModal] = useState(false) // Modal
+   const [profileModal, setProfileModal] = useState(false) // Profile Modal
 
 
    // API -> /api/authCheck
@@ -43,8 +46,8 @@ export default function Navbar({ boardTitle }: { boardTitle: string }) {
 
                {/* Project name and logo */}
                <div className='flex items-center space-x-2'>
-                  <CircleCheck className='size-6 sm:size-8 text-blue-600' />
-                  <span className=' text-xl sm:text-2xl font-bold text-gray-900'>Trello</span>
+                  <Image className='size-6 sm:size-8 text-blue-600' alt='Icon-app' width={200} height={200} src={'/app-Icon/trello.png'}></Image>
+                  <span className=' text-xl sm:text-2xl select-none font-bold text-gray-900'>Trello</span>
                </div>
 
                {/* Login buttons and Dashboard Button */}
@@ -54,9 +57,9 @@ export default function Navbar({ boardTitle }: { boardTitle: string }) {
 
                         // If the user was registered -> 
                         <div className=' flex gap-1 flex-col sm:flex-row items-end sm:items-center sm:space-y-0 sm:space-x-4'>
-                           <span className=' text-xs sm:text-base text-gray-600 hidden sm:block'>welcome {userData?.firstname}</span>
+                           <span className=' text-xs sm:text-base select-none text-gray-600 hidden sm:block'>welcome {userData?.firstname}</span>
                            <Link href="/dashboard">
-                              <Button size="sm" className=" text-xs px-3 py-5 sm:text-base">Go to dashboard<ArrowRightIcon></ArrowRightIcon></Button>
+                              <Button size="sm" className="cursor-pointer text-xs px-3 py-5 sm:text-base">Go to dashboard<ArrowRightIcon></ArrowRightIcon></Button>
                            </Link>
                         </div>
 
@@ -65,8 +68,8 @@ export default function Navbar({ boardTitle }: { boardTitle: string }) {
                         // If the user was not registered
                         <div>
                            {/* SignIn and SignUp Button */}
-                           <button onClick={() => redirect('/auth/signIn')} className=' text-sm sm:text-base px-3 py-1 ' >Sign In</button>
-                           <button onClick={() => redirect('/auth/signUp')} className=' sm:text-base px-3 py-1 text-sm bg-black text-white rounded-lg'>Sign Up</button>
+                           <button onClick={() => redirect('/auth/signIn')} className='cursor-pointer text-sm sm:text-base px-3 py-1 ' >Sign In</button>
+                           <button onClick={() => redirect('/auth/signUp')} className='cursor-pointer sm:text-base px-3 py-1 text-sm bg-black text-white rounded-lg'>Sign Up</button>
                         </div>
 
                      )
@@ -79,33 +82,55 @@ export default function Navbar({ boardTitle }: { boardTitle: string }) {
       )
    }
 
-
-
-
-
-
    // if PathName -> Dashboard
    if (isDashboardPage) {
       return (
-         <header className=" flex justify-center border bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+         <header className=" border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
             <div className=' container mx-auto flex justify-between items-center px-4 py-3 sm:py-4 '>
-
-
 
                {/*  */}
                <div className='flex items-center space-x-2 text-2xl'>
-                  <CircleCheck className='size-6 sm:size-8 text-blue-600' />
-                  <span className=' text-xl md:text-2xl font-bold text-gray-900'>Trello Clone </span>
+                  <Image className=' size-6 sm:size-8  text-blue-600' alt='Icon-app' width={200} height={200} src={'/app-Icon/trello.png'}></Image>
+                  <span className=' text-xl md:text-2xl font-bold text-gray-900'>Trello</span>
                </div>
 
-               <div className=' flex space-x-1 text-xl sm:space-x-1'>
-                  <div className=' bg-blue-600 flex justify-center text-white items-center size-10 rounded-full'>{[...userData?.firstname ?? ""][0]}</div>
-               </div>
+               {
+                  isLoading ? (<Spinner />) : (
+                     <div className=' relative flex space-x-1 text-xl sm:space-x-1'>
+                        {/* <div onClick={() => setProfileModal(!profileModal)} className=' select-none cursor-pointer hover:bg-blue-700 m-0 bg-blue-600 flex justify-center text-white items-center size-8 sm:size-12 rounded-full'>
+                           {[...userData?.firstname ?? ""][0]}
+                           <div className=' text-red-600'><X/></div>
+                        </div> */}
+                        <div onClick={() => setProfileModal(!profileModal)} className={` ${profileModal ? ' bg-red-600 hover:bg-red-700' : ' bg-blue-600 hover:bg-blue-700'} select-none cursor-pointer  m-0 flex justify-center text-white items-center size-8 sm:size-12 rounded-full`}>
+                           <p className={`${profileModal ? "hidden" : " block"}`}>
+                              {[...userData?.firstname ?? ""][0]}
+                           </p>
+                           <div className={`${profileModal ? " block" : " hidden"} text-white`}><X /></div>
+                        </div>
+
+
+                        <Card className={`${profileModal === true ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2"} absolute select-none transition-all duration-200 min-w-80 border-2 bg-white rounded-2xl p-5 top-full right-1/2 `}>
+                           <div className=' flex gap-4 items-center'>
+                              <div className=' m-0 bg-blue-600 flex justify-center text-white text-lg  items-center size-8 sm:size-12 rounded-full'>{[...userData?.firstname ?? ""][0]}</div>
+                              <div>
+                                 <p className=' sm:text-xl font-bold'>Kaveh</p>
+                                 <p className=' sm:text-lg'>Kaveh-khorshidi</p>
+                              </div>
+                           </div>
+                           <Button className="bg-red-600 cursor-pointer hover:bg-red-700">signOut</Button>
+                        </Card>
+
+
+                     </div>
+                  )
+               }
+
 
             </div>
          </header >
       )
    }
+
 
 
    // if board -> board
